75897d3e12797197328ed988037220188558fa59
Willian Sostenes <sostenes655@gmail.com>
2020-10-14 20:30:06 -0300

    Iniciando a criação das Classes

---
+++ Aula011/src/aula011/Aluno.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula011;
+
+/**
+ *
+ * @author PORTOMASTER
+ */
+public class Aluno {
+    private int matricula;
+    private String curso;
+    
+    public void cancelarMatric(){
+        
+    }
+    
+}
--- Aula011/src/aula011/Pessoa.java
+++ Aula011/src/aula011/Pessoa.java
@@ -10,5 +10,38 @@ package aula011;
  * @author PORTOMASTER
  */
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private String Sexo;
+    
+    public void fazerAniver(){
+        this.idade++;
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+ 
     
 }
